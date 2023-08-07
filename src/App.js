1d30b82da122cde9ddbcf8de3deef0e567651ccc
Anushiya <124038547+Anushiya43@users.noreply.github.com>
2023-08-07 17:45:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -46,11 +46,14 @@ export default function App() {
       ) {
         setWinner(a[1][1]);
         break;
-      } else {
+      } else if(a[0][0]!==a[0][1]!==a[0][2]!==a[1][0]!==a[1][1]!==a[1][2]!==a[2][0]!==a[2][1]!==a[2][2]!=='.'){
+        setWinner("Match Draw");
+        break;
+      }else {
         continue;
       }
     }
-  }
+    
 
   return (
     <div className="App">
